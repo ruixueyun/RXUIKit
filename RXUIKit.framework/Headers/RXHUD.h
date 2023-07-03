32204fb42831a2d4ef45648ce1e4d81882611c55
chenhan <chenhan@jixiang.cn>
2023-07-03 17:43:49 +0800

    3.3.3.1 客服域名处理

--- RXUIKit.framework/Headers/RXHUD.h
+++ RXUIKit.framework/Headers/RXHUD.h
@@ -20,15 +20,33 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (instancetype)instantce;
 
+/**
+ * 展示HUD
+ */
 + (void)showHUD;
+/**
+ * 展示HUD，可设置偏移量
+ */
 + (void)showHUDWithOffset:(CGRect)rect;
+/**
+ * 隐藏HUD
+ */
 + (void)hideHUD;
+/**
+ * 隐藏Web版HUD
+ */
 + (void)hideWebHUD;
-
+/**
+ * 添加文字loading，没有icon
+ */
 + (void)showText:(NSString *)text;
-
+/**
+ * 成功icon+文字
+ */
 + (void)showSuccessText:(NSString *)text;
-
+/**
+ * 失败icon+文字
+ */
 + (void)showErrorText:(NSString *)text;
 
 @end
