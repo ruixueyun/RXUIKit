002ca6f449e107f20b910fece3415ef4a34cc693
chenhan <chenhan@jixiang.cn>
2023-07-05 12:55:15 +0800

    3.3.3.6 新增用户中心透传参数

--- RXUIKit.framework/Headers/RXUserCenterConfig.h
+++ RXUIKit.framework/Headers/RXUserCenterConfig.h
@@ -12,23 +12,23 @@ NS_ASSUME_NONNULL_BEGIN
 @interface RXUserCenterConfig : NSObject
 
 /**
- * 透传给客服面板的参数 使用jsonString形式（客服用）
+ * 账号注销透传数据，使用jsonString形式
  */
-@property (nonatomic, strong) NSString *param_ui;
+@property (nonatomic, strong) NSString *transmit_args;
 /**
- * 用户的游戏id（客服用）
+ * 用户的游戏id
  */
 @property (nonatomic, strong) NSString *game_user_id;
 /**
- * 用户昵称，不传默认取登录nickname（客服用）
+ * 用户昵称
  */
 @property (nonatomic, strong) NSString *nickname;
 /**
- * 用户头像（客服用）
+ * 用户头像
  */
 @property (nonatomic, strong) NSString *head_img_url;
 /**
- * 在瑞雪客服系统设置的接入点名称 不填写默认为default（客服用）
+ * 在瑞雪客服系统设置的接入点名称 不填写默认为default
  */
 @property (nonatomic, strong) NSString *queue_name;
 
